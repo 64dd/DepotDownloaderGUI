ba0f9ab2fd5583826d32d629ef6efecaa120aee3
64dd <51846487+64dd@users.noreply.github.com>
2025-10-29 02:19:15 -0700

    Create folder

---
+++ DepotDownloaderClient/Program.cs
@@ -0,0 +1,11 @@
+namespace DepotDownloaderClient;
+
+static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new ClientForm());
+    }
+} 
